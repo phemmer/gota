d5ec82c6fbe1f17877f7a08d041c679dc3fb66ce
Patrick Hemmer <patrick.hemmer@gmail.com>
2019-11-10 16:59:34 -0500

    add AlgQuad interface

--- gota.go
+++ gota.go
@@ -9,5 +9,18 @@ type AlgSimple interface {
 	WarmCount() int
 }
 
+type AlgQuad interface {
+	// Add adds a new sample value to the algorithm and returns the computed value.
+	// Commonly the parameters are (high, low, close, volume).
+	Add(float64, float64, float64, float64) float64
+	// Warmed indicates whether the algorithm has enough data to generate accurate results.
+	Warmed() bool
+	// WarmCount returns the number of samples that must be provided for the algorithm to be fully "warmed".
+	WarmCount() int
+}
+
+
 // AlgSimpleConstructor is the signature accepted by various algorithms which accept any moving average, or other AlgSimple, implementation.
 type AlgSimpleConstructor func(inTimePeriod int) AlgSimple
+type AlgQuadConstructor func(inTimePeriod int) AlgQuad
+type AlgQuad2PerConstructor func(inTimePeriodShort, inTimePeriodLong int) AlgQuad
